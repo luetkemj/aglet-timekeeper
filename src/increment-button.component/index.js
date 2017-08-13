5d4dd5b378e00babcf0300b121282efb6c645695
Mark Luetke <luetkemj@gmail.com>
2017-08-12 20:04:34 -0500

    Refine buttons and transition sun moon
    - Better hit points and style for increment buttons
    - animate transition of the sun and moon

--- src/increment-button.component/index.js
+++ src/increment-button.component/index.js
@@ -6,17 +6,17 @@ import style from './style.scss';
 const IncrementButton = ({ decrement, increment, unit, duration }) => (
   <div className={style.container}>
     <button
-      className={style.incrementButton}
+      className={`${style.button} ${style.incrementButton}`}
       onClick={() => increment(moment.duration(duration, unit).asMilliseconds())}
-    />
+    ><div className={style.arrow} /></button>
     <div className={style.textWrapper}>
       <span className={style.duration}>{duration}</span>
       <span className={style.unit}>{unit.slice(0, 1)}</span>
     </div>
     <button
-      className={style.decrementButton}
+      className={`${style.button} ${style.decrementButton}`}
       onClick={() => decrement(moment.duration(duration, unit).asMilliseconds())}
-    />
+    ><div className={style.arrow} /></button>
   </div>
 );
 
